4c1727840504a4ef142084bcc2b030864cf46646
anjalitanwar946 <165695150+anjalitanwar946@users.noreply.github.com>
2024-04-17 13:46:21 +0000

    2.2 commit

--- problem_2.py
+++ problem_2.py
@@ -1,15 +1,20 @@
 def Get_name():
-    name = input()
+    name = input("Name: ")
     return name
 
 def get_tshirt():
     size=["s","m","l","xl"]
-    brands=["prada","LV","being HUman","zudio","balanciaga"]
+    brands=["Prada","LV","Being HUman","Zudio","Balanciaga"]
     user=Get_name()
+    print("BRANDS\n","Prada\n", "LV\n", "Gucci\n", "Zudio\n", "Balanciaga")
     brand_name= input("T-shirt brand you are looking for: ")
-    user_size=input("enter size: ")
-    if brand_name in brands and user_size in size:
-     print(f"hi {user},Unfortunately the brand you are looking for is available.")
+    if brand_name in brands:
+     user_size=input("Enter Size: ")
+    
+     if user_size in size: 
+       print(f"Hi {user},the brand you are looking for is available in this size.")
+     else:
+       print(f"Hi {user},Unfortunately the brand you are looking for is un available in this size.")
     else:
-     print(f"hi {user},Unfortunately the brand you are looking for is un available.")
+     print(f"Hi {user},Unfortunately the brand you are looking for is un available.")
 get_tshirt()
